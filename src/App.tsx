6ced1cd847f2d4256f653329c318d878c6b085e7
tangjiangxi <1591226504@qq.com>
2022-09-16 18:12:13 +0800

    搭建框架

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+ 
+const App = () => {
+
+  return (
+    <div>小飞棍来了</div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+ 
+import ReactDOM from "react-dom";
+import App from './App'
+
+ReactDOM.render(
+	// * react严格模式
+	// <React.StrictMode>
+			<App />
+ ,
+	// </React.StrictMode>,
+	document.getElementById("root")
+);
